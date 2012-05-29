72dbb8461cc9514fdce4f4b3dd5fab16a560ad4f
Jason Grimes <jason@grimesit.com>
2012-05-29 18:54:29 -0400

    Tweak file to make Chef deploy it.

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ $config = require 'config/local.config.php';
 
 echo '<br/><br/>';
 echo 'db_master host: ' . $config['db_master']['host'] . '<br>';
-echo 'db_master db_name: ' . $config['db_master']['db_name'] . '<br>';
+echo 'db_master dbname: ' . $config['db_master']['dbname'] . '<br>';
 
 

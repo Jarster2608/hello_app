e18a5fc9ca82d020171b8b218023e7ac3feab4ea
Jason Grimes <jason@grimesit.com>
2012-05-29 18:17:51 -0400

    Tweak to kick a Chef deploy.

--- index.php
+++ index.php
@@ -4,5 +4,6 @@ $config = require 'config/local.config.php';
 
 echo '<br/><br/>';
 echo 'db_master host: ' . $config['db_master']['host'] . '<br>';
+echo 'db_master db_name: ' . $config['db_master']['db_name'] . '<br>';
 
 
